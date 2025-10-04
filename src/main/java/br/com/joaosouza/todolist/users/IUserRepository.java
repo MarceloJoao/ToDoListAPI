ed2daf722073ef334cf6058fddf2fa2db8adbc71
Joao Marcelo <joaomarcelo586@gmail.com>
2025-10-04 20:08:43 -0300

    update project

---
+++ src/main/java/br/com/joaosouza/todolist/users/IUserRepository.java
@@ -0,0 +1,12 @@
+package br.com.joaosouza.todolist.users;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    
+    UserModel findByUsername(String username);
+}
